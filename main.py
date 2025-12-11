7015397de0e2c32f9e10f7e27f92bd292259c011
Vale <valeriaangioj@gmail.com>
2025-12-11 17:00:08 +0100

    fakestore primo commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# from requests import get
+
+# URL : str = "https://api.escuelajs.co/api/v1/products/5"
+
+# def main() -> None:
+#     res = get(URL)
+#     product = res.json()
+
+#     print(product["title"])
+#     print(product["price"])
+
+#     print(res.text)
+
+# if __name__ == "__main__":
+#     main()          
